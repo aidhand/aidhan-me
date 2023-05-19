4d65696169e6e3aa3bf548d052a020eeadfb6951
Aidhan <me@aidhan.net>
2023-05-19 13:30:08 +0800

    Automatically allow builds to both CF and Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
-
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
+
+let adapter = vercel();
+
+if (process.env.CF_PAGES) {
+  adapter = cloudflare();
+}
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
   output: "server",
-  //adapter: vercel(),
-  adapter: cloudflare(),
+  adapter: adapter,
 });
